5149a8a911d681f6c857be0b8c2d022f495b9f01
Gergely Csucs <Tevemadar@users.noreply.github.com>
2025-07-01 14:28:09 +0200

    Update windows_build.cmd

--- Publish/windows_ci/windows_build.cmd
+++ Publish/windows_ci/windows_build.cmd
@@ -1,6 +1,6 @@
 REM echo off
 rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-dir /s /b "\Program Files (x86)"|find "vcvars"
+dir /s /b "\Program Files (x86)"|find /i "vcvars"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 echo "Qmake.."
